0acca5ab2f659658362e93449496764e6835dc82
onkarbajwa004 <onkarsb64@gmail.com>
2025-10-12 21:53:30 -0400

    Changes to Coin and Obstacle

--- game/Coin.java
+++ game/Coin.java
@@ -1,5 +1,34 @@
 package game;
 
-public class Coin {
+public class Coin extends Polygon implements Collidable {
+    private static final int MOVE_SPEED = 3;
+    private int value;  
 
+     public Coin(Point[] inShape, Point inPosition, double inRotation) {
+        super(inShape, inPosition, inRotation);
+        this.value = 1; // Each coin worth 1 point
+    }
+
+
+    @Override
+    public boolean collides(Polygon other) {
+        Point[] myPoints = this.getPoints();
+        Point[] otherPoints = other.getPoints();
+        
+        // Check if any of my points are in the other polygon
+        for (Point p : myPoints) {
+            if (other.contains(p)) {
+                return true;
+            }
+        }
+        
+        // Check if any of other's points are in my polygon
+        for (Point p : otherPoints) {
+            if (this.contains(p)) {
+                return true;
+            }
+        }
+        
+        return false;
+    }
 }
--- game/Obstacle.java
+++ game/Obstacle.java
@@ -1,5 +1,35 @@
 package game;
 
-public class Obstacle {
+public class Obstacle extends Polygon implements Collidable{
+    private static final int MOVE_SPEED = 3;
 
+    public Obstacle(Point[] inShape, Point inPosition, double inRotation) {
+        super(inShape, inPosition, inRotation);
+    }
+
+     public void move() {
+        position.y += MOVE_SPEED;
+    }
+
+    @Override
+    public boolean collides(Polygon other) {
+        Point[] myPoints = this.getPoints();
+        Point[] otherPoints = other.getPoints();
+        
+        // Check if any of my points are in the other polygon
+        for (Point p : myPoints) {
+            if (other.contains(p)) {
+                return true;
+            }
+        }
+        
+        // Check if any of other's points are in my polygon
+        for (Point p : otherPoints) {
+            if (this.contains(p)) {
+                return true;
+            }
+        }
+        
+        return false;
+    }
 }
